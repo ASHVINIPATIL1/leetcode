52b98f5b174bafaee46f35c2dba42fdc48dc09a6
ASHVINI PATIL <kbtug23174@kbtcoe.org>
2026-07-30 14:43:56 +0530

    Time: 95 ms (56.31%), Space: 31 MB (46.14%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,35 @@
+import random
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Quick Select approach
+
+        # k = len(nums) - k
+
+        # def quickSelect(l, r):
+        #     pivot_idx = random.randint(l, r)
+        #     nums[pivot_idx], nums[r] = nums[r], nums[pivot_idx]
+
+        #     pivot = nums[r]
+        #     p = l
+        #     for i in range(l, r):
+        #         if nums[i] <= pivot:
+        #             nums[p], nums[i] = nums[i], nums[p]
+        #             p += 1
+        #     nums[p], nums[r] = nums[r], nums[p]
+
+        #     if p > k: return quickSelect(l, p - 1)
+        #     elif p < k: return quickSelect(p + 1, r)
+        #     else: return nums[p]
+            
+        # return quickSelect(0, len(nums) - 1)
+
+        # Min Heap time - O(n log k) , space - O(k)
+        heap = []
+
+        for num in nums:
+            heapq.heappush(heap, num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
