8076efb07d35755f7bc78669a7186d46e1e92071
ASHVINI PATIL <kbtug23174@kbtcoe.org>
2026-08-08 00:00:59 +0530

    Time: 0 ms (100%), Space: 19.2 MB (79.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,8 +3,8 @@ class Solution:
         for i in range(len(nums)):
             if (i == 0 or nums[i - 1] < nums[i]) and (i == (len(nums) - 1) or nums[i] > nums[i + 1]):
                 return i
-            if nums[i] > nums[i - 1] and nums[i] > nums[i+1]:
-                return i
+            # if nums[i] > nums[i - 1] and nums[i] > nums[i+1]:
+            #     return i
         # peak = 0
         # st = 1
         # end = len(nums) - 2
