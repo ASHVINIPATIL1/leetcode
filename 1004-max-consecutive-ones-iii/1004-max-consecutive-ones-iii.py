ea23fad0727b56f80c118e36f718a685c1bd5c0d
ASHVINI PATIL <kbtug23174@kbtcoe.org>
2026-09-08 23:24:55 +0530

    Time: 73 ms (14.68%), Space: 22.2 MB (22.59%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int: 
+
+        n = len(nums)
+        zeros = 0
+        maxLen = 0 
+
+        l = 0
+        r = 0
+
+        while r < n:
+            if nums[r] == 0:
+                zeros += 1
+            while zeros > k:
+                if nums[l] == 0:
+                    zeros -= 1
+                l += 1
+            if zeros <= k:
+                cLen = r -l + 1
+                maxLen = max(cLen, maxLen)
+            r += 1 
+        return maxLen
+
+
+        
